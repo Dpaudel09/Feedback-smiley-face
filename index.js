4ceb6f4c2dd4ad8092ae554a2ead47e93121f7ed
Dpaudel09 <deepakpaudel897@gmail.com>
2024-06-26 15:36:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendButton = document.getElementById("send");
+const panel = document.getElementById("panel");
+let selectedRating = "Satisfied";
+
+const removeActive = () => {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+};
